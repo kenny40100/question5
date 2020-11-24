4cd903a313b04227783d556518e6a6752abff21a
kenny40100 <71364976+kenny40100@users.noreply.github.com>
2020-11-24 15:31:41 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,21 @@
+let y = 0
+let x = 0
 input.onButtonPressed(Button.A, function () {
     y += 1
 })
+input.onButtonPressed(Button.AB, function () {
+    x = 10
+    while (x > 0) {
+        basic.showNumber(x)
+        x += -1
+    }
+    basic.clearScreen()
+    if (y >= 50) {
+        basic.showString("WIN")
+    } else {
+        basic.showString("LOSE")
+    }
+})
 input.onButtonPressed(Button.B, function () {
     y += 1
-})
-let y = 0
-let x = 10
-while (x > 0) {
-    basic.showNumber(x)
-    x += -1
-}
-basic.clearScreen()
-if (y >= 50) {
-    basic.showString("WIN")
-} else {
-    basic.showString("LOSE")
-}
-basic.forever(function () {
-	
 })
